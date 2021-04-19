beddc7715801cfdc902bbdd16bc705719b8b9bfc
Dev-jiseong <57554654+Dev-jiseong@users.noreply.github.com>
2021-04-19 17:26:05 +0900

    Update Admin.java

--- soprj/Admin.java
+++ soprj/Admin.java
@@ -1,4 +1,4 @@
-package soprj;
+package tmp;
 import java.util.ArrayList;
 
 public class Admin {
@@ -11,18 +11,18 @@ public class Admin {
 		bList = new ArrayList<Book>();
 	}
 	
-	public void addUser(User user) { //���� ȸ�����Խ� ���� �߰�
-		System.out.println(user.getName() + "���� ȸ�������� �Ϸ�Ǿ����ϴ�.");
+	public void addUser(User user) { //유저 회원가입시 유저 추가
+		System.out.println(user.getName() + "님의 회원가입이 완료되었습니다.");
 		uList.add(user);
-		System.out.println("����Ʈ�� �߰��Ϸ�: " + uList.get(uList.size() - 1).getName()); //����Ʈ�� �� �߰��ƴ��� Ȯ�ο�
+		System.out.println("리스트에 추가완료: " + uList.get(uList.size() - 1).getName()); //리스트에 잘 추가됐는지 확인용
 	}
-	public void addBook(Book book) { //���� ��Ͻ� ���� �߰�
+	public void addBook(Book book) { //도서 등록시 도서 추가
 		bList.add(book);
 	}
-	public void removeUser() { //ȸ�� ����
+	public void removeUser() { //회원 삭제
 		
 	}
-	public void removeBook() { //���� ����
+	public void removeBook() { //도서 삭제
 		
 	}
 	public ArrayList<User> getUserList() {
